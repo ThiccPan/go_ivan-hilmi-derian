33977105603d23b5d25d49c16b8ec608798fdfbf
ThiccPan <ihilmiderian@gmail.com>
2023-04-08 10:08:09 +0700

    test for createUser func

--- 23_Unit-test/praktikum/praktikum-2/controller/user_controller_test.go
+++ 23_Unit-test/praktikum/praktikum-2/controller/user_controller_test.go
@@ -4,14 +4,25 @@ import (
 	"23_Unit-test/praktikum/praktikum-2/config"
 	"23_Unit-test/praktikum/praktikum-2/model"
 	"encoding/json"
+
 	"net/http"
 	"net/http/httptest"
 	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/labstack/echo"
 	"github.com/stretchr/testify/assert"
 )
+
+type testCase struct {
+	name       string
+	path       string
+	param      string
+	body       string
+	expectCode int
+	sizeData   int
+}
 
 type UsersResponse struct {
 	Message string
@@ -43,36 +54,33 @@ func InsertDataUserForGetUsers() error {
 	return nil
 }
 
+func initTestEnv(req *http.Request) (*httptest.ResponseRecorder, echo.Context) {
+	e := InitEchoTestAPI()
+	InsertDataUserForGetUsers()
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	return rec, c
+}
+
 func TestGetUsersController(t *testing.T) {
-	var testCases = []struct {
-		name       string
-		path       string
-		expectCode int
-		sizeData   int
-	}{
+	testCases := []testCase{
 		{
-			name:       "get user normal",
+			name:       "(success) get list of user",
 			path:       "/users",
 			expectCode: http.StatusOK,
 			sizeData:   1,
 		},
 	}
 
-	e := InitEchoTestAPI()
-	InsertDataUserForGetUsers()
 	req := httptest.NewRequest(http.MethodGet, "/", nil)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
+	rec, c := initTestEnv(req)
 
 	for _, testCase := range testCases {
 		c.SetPath(testCase.path)
-
 		if assert.NoError(t, GetUsersController(c)) {
 			assert.Equal(t, testCase.expectCode, rec.Code)
 			body := rec.Body.String()
 
-			// open file
-			// convert struct
 			var user UsersResponse
 			err := json.Unmarshal([]byte(body), &user)
 
@@ -85,37 +93,26 @@ func TestGetUsersController(t *testing.T) {
 }
 
 func TestGetUserController(t *testing.T) {
-	var testCases = []struct {
-		name       string
-		path       string
-		param      string
-		expectCode int
-	}{
+	var testCases = []testCase{
 		{
-			name:       "get user normal",
+			name:       "(success) get user with id",
 			path:       "/users",
 			param:      "1",
 			expectCode: http.StatusOK,
 		},
 	}
 
-	e := InitEchoTestAPI()
-	InsertDataUserForGetUsers()
 	req := httptest.NewRequest(http.MethodGet, "/", nil)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
+	rec, c := initTestEnv(req)
 
 	for _, testCase := range testCases {
 		c.SetPath(testCase.path)
 		c.SetParamNames("id")
 		c.SetParamValues(testCase.param)
-
 		if assert.NoError(t, GetUserController(c)) {
 			assert.Equal(t, testCase.expectCode, rec.Code)
 			body := rec.Body.String()
 
-			// open file
-			// convert struct
 			var user UserResponse
 			err := json.Unmarshal([]byte(body), &user)
 			t.Log(user.User)
@@ -124,6 +121,40 @@ func TestGetUserController(t *testing.T) {
 				assert.Error(t, err, "error")
 			}
 			assert.Equal(t, testCase.param, strconv.Itoa(int(user.User.ID)))
+		}
+	}
+}
+
+func TestCreateUserController(t *testing.T) {
+	var testCases = []testCase{
+		{
+			name:       "create user",
+			path:       "/users",
+			body:       `{"name":"Alta","email":"alta@gmail.com","password":"123"}`,
+			expectCode: http.StatusOK,
+		},
+	}
+
+	for _, testCase := range testCases {
+
+		// create request, recorder and test env for each testcase because every req body is different
+		req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(testCase.body))
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec, c := initTestEnv(req)
+
+		c.SetPath(testCase.path)
+		if assert.NoError(t, CreateUserController(c)) {
+			assert.Equal(t, testCase.expectCode, rec.Code)
+			body := rec.Body.String()
+			t.Log(req.Body.Close())
+
+			var user UserResponse
+			err := json.Unmarshal([]byte(body), &user)
+
+			if err != nil {
+				assert.Error(t, err, "error")
+			}
+			assert.Equal(t, "Alta", user.User.Name)
 		}
 	}
 }
