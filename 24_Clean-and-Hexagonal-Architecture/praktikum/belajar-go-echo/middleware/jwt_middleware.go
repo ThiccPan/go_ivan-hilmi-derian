803b425c0ba28889ca4f63350e4127ab62e1c481
ThiccPan <ihilmiderian@gmail.com>
2023-04-13 07:16:04 +0700

    refactor jwt middleware

--- 24_Clean-and-Hexagonal-Architecture/praktikum/belajar-go-echo/middleware/jwt_middleware.go
+++ 24_Clean-and-Hexagonal-Architecture/praktikum/belajar-go-echo/middleware/jwt_middleware.go
@@ -7,6 +7,35 @@ import (
 	"github.com/golang-jwt/jwt/v4"
 )
 
+type AuthJWT interface {
+	GenerateToken(email string) (string, error)
+}
+
+type authJWT struct {
+	secret string
+}
+
+func NewAuthJWT() *authJWT {
+	return &authJWT{
+		constants.JWT_SECRET,
+	}
+}
+
+func (aj *authJWT) GenerateToken(email string) (string, error) {
+	claims := &jwtCustomClaims{
+		email,
+		jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 72)),
+		},
+	}
+
+	// Create token with claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// Generate encoded token and send it as response.
+	return token.SignedString([]byte(constants.JWT_SECRET))
+}
+
 type jwtCustomClaims struct {
 	Email string `json:"email"`
 	jwt.RegisteredClaims
